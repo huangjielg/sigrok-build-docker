3d58e78830d49c67553212337c1be607c0865108
huangjielg <huangjielg@gmail.com>
2023-03-15 09:05:12 +0800

    Create build-mxe.sh

---
+++ .github/build-mxe.sh
@@ -0,0 +1,48 @@
+apt-get install -y --no-install-recommends \
+    autoconf \
+    automake \
+    autopoint \
+    bash \
+    bison \
+    bzip2 \
+    flex \
+    g++ \
+    g++-multilib \
+    gettext \
+    git \
+    gperf \
+    intltool \
+    libc6-dev-i386 \
+    libgdk-pixbuf2.0-dev \
+    libltdl-dev \
+    libgl-dev \
+    libssl-dev \
+    libtool-bin \
+    libxml-parser-perl \
+    lzip \
+    make \
+    openssl \
+    p7zip-full \
+    patch \
+    perl \
+    python3 \
+    python3-mako \
+    python3-pkg-resources \
+    ruby \
+    sed \
+    unzip \
+    wget \
+    xz-utils
+    
+git clone --depth 1 https://github.com/mxe/mxe.git 
+ln -s /usr/bin/python3 /usr/bin/python
+make -C mxe  MXE_TARGETS=x86_64-w64-mingw32.static.posix \
+   MXE_PLUGIN_DIRS=plugins/examples/qt5-freeze \
+   ccache
+  
+   
+#make -C mxe  MXE_TARGETS=x86_64-w64-mingw32.static.posix \
+#   MXE_PLUGIN_DIRS=plugins/examples/qt5-freeze \
+#   gcc glib libzip libusb1 libftdi1 hidapi glibmm qtbase qtimageformats \
+#   qtsvg qttranslations boost check gendef libieee1284 \
+#   qtbase_CONFIGURE_OPTS='-no-sql-mysql'   
